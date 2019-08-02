407b8e3a5318d3501828f9d0632937dca20a762a
Vince <vincentguan23@gmail.com>
2019-08-02 17:36:18 -0400

    fixed bug where stream was not terminating after last line of .txt file

--- src/YelpAnalysis.java
+++ src/YelpAnalysis.java
@@ -8,14 +8,18 @@ import java.util.*;
 public class YelpAnalysis {
 
     //map where the keys are every word that appears in any review, and the value is how many documents they appear in
-    //basically a dictionary of word frequencies
+    //i.e. basically a dictionary of word frequencies
     private Map<String, Integer> dictionary = new HashMap<>();
+    SortByReviewCharCount srcc = new SortByReviewCharCount();
+    //priority Queue representing every business in the .txt file
+    private Queue<Business> businesses = new PriorityQueue<>(srcc.reversed());
 
 
     //testing
     public static void main(String[] args) {
         YelpAnalysis yp = new YelpAnalysis();
         yp.txtToString();
+        System.out.println(yp.dictionary);
     }
 
     //given a .txt file, this constructs one String for each Business in the .txt file, and then
@@ -28,29 +32,35 @@ public class YelpAnalysis {
             in = new FileInputStream("yelpDatasetParsed_short.txt");
             in = new BufferedInputStream(in);
             while (true) {
+                int res = in.read();
+                if (res == -1) {
+                    break;
+                }
                 //reads the next character in the file
-                char result = (char) in.read();
+                char result = (char) res;
+                // if there are no more characters in the file, then result will be -1, and we exit the loop
                 //since the format of the file is {, Business info, }, {, Business info, }, ...
                 // we can build a string representing each business by looking inside the brackets
                 if (result == '{') {
+                    System.out.println("beg");
                     continue;
                 }
                 if (result == '}') {
                     //construct a business with sb
-                    strToBusiness(sb.toString());
+                    businesses.add(strToBusiness(sb.toString()));
                     sb = new StringBuilder();
                     continue;
                 } else {
                     //we are interested in what is between the brackets
                     sb.append(result);
                 }
-                // if there are no more characters in the file, then result will be -1, and we exit the loop
-                if (result == -1) break;
+
             }
         } catch (IOException e) {
             System.out.println("couldn't find file");
         } finally {
             try {
+                System.out.println("trynna close");
                 in.close();
             } catch (Exception e) {
                 System.out.println("could't close file");
@@ -80,6 +90,7 @@ public class YelpAnalysis {
             } else {
                 reviews = BusFieldStrings.get(x).trim();
                 reviewCharCount = charcount(reviews);
+                this.dictionaryhelper(reviews);
                 b = new Business(businessID, businessName, businessAddress, reviews, reviewCharCount, null);
             }
         }
@@ -90,6 +101,28 @@ public class YelpAnalysis {
     //helper function that counts how many characters there are in a business' reviews
     public int charcount(String reviews) {
         return reviews.length();
+    }
+
+    public void dictionaryhelper(String reviews) {
+        //split the reviews into a list of individual words
+        List<String> wordsinreviews = Arrays.asList(reviews.split(" "));
+        // this removes duplicate words in the list of words
+        List<String> wordsinreviews_nodups = new ArrayList<>();
+        for (String s : wordsinreviews) {
+            if (!wordsinreviews_nodups.contains(s.toLowerCase())) {
+                wordsinreviews_nodups.add(s.toLowerCase());
+            }
+        }
+        //now we go through the list of unique words and adjust the dictionary correspondingly
+        for (String s : wordsinreviews_nodups) {
+            //if the dictionary doesn't already have the word, then add it to the dictionary with value 1
+            if (!dictionary.containsKey(s.toLowerCase())) {
+                dictionary.put(s.toLowerCase(), new Integer(1));
+            } else {
+                //otherwise, increment the value associated to the word by 1
+                dictionary.put(s.toLowerCase(), new Integer(dictionary.get(s).intValue() + 1));
+            }
+        }
     }
 
 
