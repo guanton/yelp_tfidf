131e684c726d4114d4f206f560500ca3f12263a3
Vince <vincentguan23@gmail.com>
2019-08-02 00:15:00 -0400

    working parser that takes in a .txt file and creates a Business object for each Business in the .txt file

---
+++ src/Business.java
@@ -0,0 +1,52 @@
+import java.util.Map;
+
+public class Business {
+    //each business in the dataset has an ID, a name, an address, a list of reviews (String), a number of reviews,
+    //and finally, an tf-idf score associated to each word that appears in its reviews
+    //tf-idf is the frequency of a word in a business's reviews divided by the number of other businesses whose reviews
+    //contain at least one mention of the word
+    //we will use these tf-idf scores to generate accurate tags that reflect each business
+    String businessID;
+    String businessName;
+    String businessAddress;
+    String reviews;
+    int reviewCharCount;
+    Map<String, Double> tfidfmap;
+
+    public Business(String businessID, String businessName, String businessAddress, String reviews, int reviewCharCount,
+                    Map<String, Double> idfmap) {
+        this.businessID = businessID;
+        this.businessName = businessName;
+        this.businessAddress = businessAddress;
+        this.reviews = reviews;
+        this.reviewCharCount = reviewCharCount;
+        this.tfidfmap = idfmap;
+    }
+
+    //converts the map of words with their tfidf scores to a string
+    public String maptostring(Map<String, Double> tfidfmap) {
+        String s = "";
+        int x = 1;
+        for (Map.Entry<String,Double> entry : tfidfmap.entrySet()) {
+            s = s + "(" + entry.getKey() + ", " + entry.getValue() + "), ";
+            if (x % 6 == 0) {
+                s=s+"\n";
+            }
+            x++;
+        }
+        return s;
+    }
+
+    public String toString() {
+        return "-------------------------------------------------------------------------------\n"
+                + "Business ID: " + businessID + "\n"
+                + "Business Name: " + businessName + "\n"
+                + "Business Address: " + businessAddress + "\n"
+                + "Reviews: " + reviews + "\n"
+                + "Character Count: " + reviewCharCount + "\n"
+                + "idf scores: " + "NA"; //update later to: maptostring(tfidfmap)
+    }
+
+
+
+}
