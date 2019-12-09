7ab3601232203a94cf5ad157c04ded5fc764f425
Vince <vincentguan23@gmail.com>
2019-12-09 12:27:58 -0800

    basic groundwork for gui

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,27 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SearchEngine {
     private JButton searchButton;
     private JPanel panelMain;
+    private JTextField textField;
+    private JPanel pan;
     private YelpAnalysis yp;
 
 
     public SearchEngine() {
-
+        yp = new YelpAnalysis();
         //search button
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                yp.txtToString();
+                for (Business b: yp.filtertop10()) {
+                    b.settfidfmap(yp.top30words(b));
+                    System.out.println(b);
+                }
+            }
+        });
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String query = textField.getText();
+                System.out.println(query);
             }
         });
     }
 
     public void display(){
         JFrame f = new JFrame("Yelp Search");
-        f.setContentPane(new SearchEngine().panelMain);
+        f.setContentPane(new SearchEngine().pan);
         f.setSize(800, 600);
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setVisible(true);
